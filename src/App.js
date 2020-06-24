248cdb7b498dbaa7b69abbbd961b52540c002b0a
ahsinrasheed5 <65343061+ahsinrasheed5@users.noreply.github.com>
2020-06-24 22:00:36 +0100

    used of useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let data = {title: "Waiting for Data from server"};
+  const [todo , setTodo] = useState(data);
+  const [isData , setData]  = useState(false);
+  const [isFetching , setFetching] = useState(false);
+
+
+  
+  useEffect( ()=>{
+    async function fetchData(){
+      setFetching(true);
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+      console.log( "Response = " ,response);
+      let data2 = await response.json()
+      setTodo(data2);
+      setFetching(false);
+      console.log( "DAtA = ", todo);
+    }
+    fetchData();
+  },[isData]);
+
+  if(isFetching){
+    return <div>Data Loading...</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello Fetch 
+      <span> title: {todo.title}</span>
     </div>
   );
 }
