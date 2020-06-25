599f0e6e64bb9f58caac6bf7fcc797b32365ce5f
ahsinrasheed5 <65343061+ahsinrasheed5@users.noreply.github.com>
2020-06-25 19:55:28 +0100

    Use of useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React, { useEffect } from 'react';
+import './App.css';
 
 function App(){
 
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+  })
+
+
+
   return (
-    
-    
-    console.log("data")
-  
+    <div className='App'>
+      <h1>Hello World </h1>
+    </div>
     );
 
 }
